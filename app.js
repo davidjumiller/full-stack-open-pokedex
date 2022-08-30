3afc18c7ed85a8a1187ecdd567d43fd25a0e8b82
davidjumiller <davidjumiller@gmail.com>
2022-08-30 12:14:31 -0700

    added healthcheck for deployment pipeline

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ require('dotenv').config()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
